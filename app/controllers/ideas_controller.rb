3d52444a597bb4ae9c32f111e01323defbcc4630
andrewetobin <andrewetobin@gmail.com>
2018-09-02 13:43:29 -0600

    adds new and create to ideas controller

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,7 +1,23 @@
 class IdeasController < ApplicationController
 
   def index
-    @ideas = Idea.all 
+    @ideas = Idea.all
+  end
+
+  def show
+    @idea =  Idea.find(params[:id])
+  end
+
+  def new
+    @idea = Idea.new
+  end
+
+  def create
+    @idea = Idea.new
+    @idea.title = params[:idea][:title]
+    @idea.body = params[:idea][:body]
+    @idea.save
+    redirect_to idea_path(@idea)
   end
 
 
